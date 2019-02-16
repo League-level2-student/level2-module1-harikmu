823b911206caa995377d01fbdb7e9f519da8fb5b
league <league@wts-imac-11.attlocal.net>
2019-02-15 17:00:00 -0800

    array list demo guest book intro to array lists

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,7 +1,42 @@
 package intro_to_array_lists;
 
-public class GuestBook {
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+public class GuestBook implements ActionListener{
+	public static void main(String[] args) throws Exception {
+		GuestBook guestbook = new GuestBook();
+		guestbook.makeButtons();
+	}
+	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names".
+		JFrame f = new JFrame();
+		JButton b1 = new JButton();
+		JButton b2 = new JButton();
+		JPanel p = new JPanel();
+		private void makeButtons() {
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(125, 90);
+		f.add(p);
+		p.add(b1);
+		p.add(b2);
+		b1.setText("add name");
+		b2.setText("view names");
+		b1.addActionListener(this);
+		b2.addActionListener(this);
+		}
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
@@ -9,4 +44,18 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
-}
+	ArrayList<String> names = new ArrayList<String>();
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource()==b1) {
+		   String n = JOptionPane.showInputDialog("enter a name. do this four times");
+		   names.add(n);
+		}
+		if(e.getSource()==b2) {
+				JOptionPane.showMessageDialog(null, "guest #1: " + names.get(0) + ", guest #2: " + names.get(1) + ", guest #3: " + names.get(2) + ", guest #4: " + names.get(3));
+			}
+		}
+		
+		
+	}
+
--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,20 +1,41 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
+
+import org.omg.Messaging.SyncScopeHelper;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-		
+		ArrayList<String> strings = new ArrayList<String>();
 		//2. Add five Strings to your list
-		
+		strings.add("a");
+		strings.add("b");
+		strings.add("c");
+		strings.add("d");
+		strings.add("e");
 		//3. Print all the Strings using a standard for-loop
-		
+		for (int i = 0; i < strings.size(); i++) {
+			String s = strings.get(i);
+			System.out.println("standard for-loop: " + s);
+		}
 		//4. Print all the Strings using a for-each loop
-		
+		for (String s : strings) {
+			System.out.println("for-each loop: " + s);
+		}
 		//5. Print only the even numbered elements in the list.
-		
+		for (int i = 0; i < strings.size(); i++) {
+			if (i % 2 == 1 ) {
+			String s = strings.get(i);
+			System.out.println("even numbered: " + s);
+			}
+		}
 		//6. Print all the Strings in reverse order.
-		
+		for (int i = strings.size() + -1; i >= 0; i--) {
+			String s = strings.get(i);
+			System.out.println("reverse order: " + s);
+		}
 		//7. Print only the Strings that have the letter 'e' in them.
 	}
 }
